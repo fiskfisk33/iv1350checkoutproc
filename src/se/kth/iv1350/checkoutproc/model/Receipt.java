8638641a258def60accc8f7a009c0a4e5319a0a6
Jonathan Johnson <git@tehfisk.se>
2023-05-01 16:57:57 +0200

    more implementation

--- src/se/kth/iv1350/checkoutproc/model/Receipt.java
+++ src/se/kth/iv1350/checkoutproc/model/Receipt.java
@@ -1,8 +1,5 @@
 package se.kth.iv1350.checkoutproc.model;
 
-import java.math.BigDecimal;
-import java.time.Instant;
-
 public class Receipt extends SaleInfoDTO {
         public Receipt(SaleInfoDTO saleInfo) {
                 //TODO return errors if payment and/or change are missing

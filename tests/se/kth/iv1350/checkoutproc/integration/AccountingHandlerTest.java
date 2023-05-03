80b7a0dde478e06fd27636b6cd7d02b6c90a2c7b
Jonathan Johnson <git@tehfisk.se>
2023-05-03 10:44:39 +0200

    AccountingHandlerTest implemented

---
+++ tests/se/kth/iv1350/checkoutproc/integration/AccountingHandlerTest.java
@@ -0,0 +1,22 @@
+package se.kth.iv1350.checkoutproc.integration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountingHandlerTest {
+
+        @BeforeEach
+        void setUp() {
+        }
+
+        @AfterEach
+        void tearDown() {
+        }
+
+        @Test
+        void logSale() {
+        }
+}
